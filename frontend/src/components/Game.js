dc53e4e16bbed8f2d3da017e4b7bc867a3e0c962
Daniel <123475923+padmored@users.noreply.github.com>
2023-11-21 11:40:55 +0000

    Input box added to game

--- frontend/src/components/Game.js
+++ frontend/src/components/Game.js
@@ -1,11 +1,19 @@
 import "../App.css";
+import React, { useState } from 'react';
+
 function Game() {
 
     const timer = "TIMER"
     const keyword = "Rabbit"
     const language = "French"
-    
-    const answer = ["_ _ _ _ _"]
+
+    const [typedWord, setTypedWord] = useState(Array(keyword.length).fill(''));
+      
+    const handleInputChange = (index, value) => {
+        const newTypedWord = [...typedWord];
+        newTypedWord[index] = value;
+        setTypedWord(newTypedWord);
+    };
 
     return (
         <div className="Game">
@@ -15,7 +23,11 @@ function Game() {
             <div className="Question">
                 <h1>What is the word for <span className="Keyword">{keyword}</span> in {language} ?</h1>
             </div>
-            <div className="Answer-box"><span className="Answer">{ answer }</span></div>
+            <div>
+                {typedWord.map((letter, index) => (
+                <input key={index} type="text" value={letter} maxLength={1} onChange={(e) => handleInputChange(index, e.target.value)}/>
+                ))}
+            </div>
             <button class="button button2">Hint</button>
 
         </div>
