882d127f2f2425017d9308c04f5198b313771ecc
Daniel <123475923+padmored@users.noreply.github.com>
2023-11-21 00:49:37 +0000

    flags added and color overhaul

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,12 @@
 import './App.css';
-import Sidebar from './components/Sidebar';
 import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
 import Game from './components/Game';
 
 function App() {
   return (
     <div className="App">
-      <div className="Nav">
-        <Navbar/>
-      </div>
+      <Navbar/>
       <div className="Main">
         <Sidebar/>
         <Game/>
--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,4 +1,6 @@
 import "../App.css";
+import Flag from 'react-world-flags';
+
 function Sidebar() {
 
     const languages = ["Irish", "French", "Spanish"];
@@ -7,9 +9,18 @@ function Sidebar() {
         <div className="Sidebar">
             <h1>Languages</h1>
             <div className="Sidebar-list">
-                <button class="button button1">{ languages[0] }</button>
-                <button class="button button1">{ languages[1] }</button>
-                <button class="button button1">{ languages[2] }</button>
+                <button class="button button1">
+                    <Flag class="flag" code="irl" height="32" />
+                    <h3>{ languages[0] }</h3>
+                </button>
+                <button class="button button1">
+                    <Flag class="flag" code="fr"  height="32" />
+                    <h3>{ languages[1] }</h3>
+                </button>
+                <button class="button button1">
+                    <Flag class="flag" code="es"  height="32" />
+                    <h3>{ languages[2] }</h3>
+                </button>
             </div>
             <div className="Sidebar-list">
                 <button class="button button2">Statistics</button>
