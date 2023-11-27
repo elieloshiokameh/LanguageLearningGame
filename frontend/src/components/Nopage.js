fc483aae42986120ad1cf8db96cd06b662ac7b1c
Eliel Oshiokameh <oshiokae@tcd.ie>
2023-11-27 01:53:52 +0000

    altered error page

--- frontend/src/components/Nopage.js
+++ frontend/src/components/Nopage.js
@@ -9,16 +9,29 @@ function Nopage() {
                 style=
                 {{
                     display: "flex",
-                    alignItems: "center",
                     justifyContent: "center",
                     color: "red",
                 }}>
-                    <h1>
+                    <h1 style={{fontSize: "100px"}}>
                         404 Error
                     </h1>
             </div>
             <div className="buttons">
                 <Link to="/">menu</Link>
+            </div>
+            <div>
+                <h2
+                    style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        color: "grey",
+                    }}
+                >
+                    <div>
+                            <p style={{display: "flex", justifyContent: "center",}}>Uh-Oh...</p>
+                            <p>There seems to be a problem. Please return to menu.</p>
+                    </div>
+                </h2>
             </div>
         </div>
     );
