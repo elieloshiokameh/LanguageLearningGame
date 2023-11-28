a9db208b47119a7869c236f29aa42409a857267f
Daniel Farushev <farushed@tcd.ie>
2023-11-28 03:07:07 +0000

    Add jwt in OAuth2 success handler
    Also create User if necessary

--- backend/src/main/java/com/languagegame/security/oauth2/OAuth2AuthenticationSuccessHandler.java
+++ backend/src/main/java/com/languagegame/security/oauth2/OAuth2AuthenticationSuccessHandler.java
@@ -1,23 +1,70 @@
 package com.languagegame.security.oauth2;
 
-import jakarta.servlet.FilterChain;
+import com.languagegame.domain.User;
+import com.languagegame.repository.UserRepository;
+import com.languagegame.security.jwt.JwtUtils;
+import com.languagegame.security.service.UserDetailsImpl;
+import com.languagegame.security.service.UserDetailsServiceImpl;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseCookie;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
 @Component
 public class OAuth2AuthenticationSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
+    @Autowired
+    JwtUtils jwtUtils;
+    @Autowired
+    UserRepository userRepo;
+    @Autowired
+    UserDetailsServiceImpl userDetailsService;
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
         System.out.println("SUCCESS HANDLER authentication = " + authentication);
-        System.out.println("SUCCESS HANDLER email = " + ((OAuth2User)authentication.getPrincipal()).getAttribute("email"));
+
+        String name = ((OAuth2User) authentication.getPrincipal()).getAttribute("name");
+        String email = ((OAuth2User)authentication.getPrincipal()).getAttribute("email");
+        System.out.println("SUCCESS HANDLER name = " + name);
+        System.out.println("SUCCESS HANDLER email = " + email);
+
+
+        User user = userRepo.findByUsername(name).orElse(null);
+        if (user == null) {
+            user = new User(name, email,"OAUTH2");
+            userRepo.save(user);
+        } else if (!user.getEmail().equals(email) || !user.getPassword().equals("OAUTH2")) {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Not a valid account");
+            return;
+        }
+
+        UserDetailsImpl userDetails = UserDetailsImpl.build(user);
+
+        UsernamePasswordAuthenticationToken newAuthentication =
+                new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+
+        newAuthentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+        SecurityContextHolder.getContext().setAuthentication(newAuthentication);
+
+        // Add our new jwt cookie
+        ResponseCookie jwtCookie = jwtUtils.generateJwtCookie(userDetails);
+        response.addHeader("Set-Cookie", jwtCookie.toString());
+//        // And remove the oauth2 one!
+//        ResponseCookie jsCookie = ResponseCookie.from("JSESSION", "").maxAge(0).path("/").build();
+//        response.addHeader("Set-Cookie", jsCookie.toString());
+
         super.onAuthenticationSuccess(request, response, authentication);
     }
 }
